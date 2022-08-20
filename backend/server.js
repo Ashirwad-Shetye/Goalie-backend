919f5d57b784ce45744348a2b30c494032cb1119
Ashirwad P Shetye <ashirwadpramodshetye@gmail.com>
2022-08-21 02:56:19 +0530

    backend started

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
+const port = process.env.PORT || 5000;
+const colors = require('colors');
+const { connectDB } = require('./configs/db');
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/api/goals', require('./routes/goalRoutes'));
+
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
