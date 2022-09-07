35a168667fbe43c71baa076a0e56120863395a37
djs2082 <dilipjoshis98@gmail.com>
2022-09-07 13:25:00 +0530

    added a feature for sending email from server and added some email templates

---
+++ backend/services/email.js
@@ -0,0 +1,30 @@
+const nodemailer = require('nodemailer');
+      let transporter = nodemailer.createTransport({
+            service: 'Gmail',
+             auth: {
+                 user: process.env.EMAIL_ID,
+                 pass: process.env.EMAIL_PASSWORD
+             }
+     })
+
+
+const sendEmail = async (message,receiverMail) =>{
+    let messageData = {
+            from: process.env.EMAIL_ID,
+            to: receiverMail,
+            subject: message.subject,
+            text: message.body
+    }
+    transporter.sendMail(messageData,(err,info) => {
+        if(err){
+            console.log(err)
+        }
+        else{
+            console.log(info)
+        }
+    })
+}
+
+module.exports = {
+    sendEmail
+}
