6fdc08c38e4ab9978d2dfb98fa7d60b41e85f40a
Ashirwad P Shetye <ashirwadpramodshetye@gmail.com>
2023-02-27 02:20:28 +0530

    avatar response added

--- backend/configs/emailTemplates.js
+++ backend/configs/emailTemplates.js
@@ -1,32 +1,49 @@
-const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-
+const month = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 
 const WELCOME_EMAIL = (userName) => {
-    return({
+  return {
     type: "WELCOME_EMAIL",
-    subject: "Welcome To Goalie, Now Accompolish all your tasks on your finges!",
-    body: `Hi ${userName} welcome to Goalie, using goalie you can add all your goals in the app and keep the track of all goals of yours`
-})
-}
+    subject:
+      "Welcome To Goalie, Now Accompolish all your tasks on your fingers!",
+    body: `Hi ${userName} welcome to Goalie, using goalie you can add all your goals in the app and keep the track of all goals of yours`,
+  };
+};
 
 const GOAL_ADDED = (userName, goalTitle, lastDate) => {
-    return({
-        type: "GOAL_ADDED",
-        subject: "Congratulations! You added new goal",
-        body:   `Hi ${userName}, congratulations for adding your new goal. Your Goal title is ${goalTitle}, you promised to finish this goal by ${new Date(lastDate).getDate()}-${month[new Date(lastDate).getMonth()]}-${new Date(lastDate).getFullYear()}. We Hope you will finish this task before time and we wish you all the best for future journey of you on this jurney`
-    })
-}
+  return {
+    type: "GOAL_ADDED",
+    subject: "Congratulations! You added new goal",
+    body: `Hi ${userName}, congratulations for adding your new goal. Your Goal title is ${goalTitle}, you promised to finish this goal by ${new Date(
+      lastDate
+    ).getDate()}-${month[new Date(lastDate).getMonth()]}-${new Date(
+      lastDate
+    ).getFullYear()}. We Hope you will finish this task before time and we wish you all the best for future journey of you on this jurney`,
+  };
+};
 
 const REMAIND_GOAL = (userName, goalTitle) => {
-    return({
-        type: "REMAIND_GOAL",
-        subject: "Remainder! you have a pending goal",
-        body: `Hi ${userName} this mail is to remaind you that you have a pending goal to finish, title of goal is ${goalTitle}`
-    })
-}
+  return {
+    type: "REMAIND_GOAL",
+    subject: "Remainder! you have a pending goal",
+    body: `Hi ${userName} this mail is to remaind you that you have a pending goal to finish, title of goal is ${goalTitle}`,
+  };
+};
 
 module.exports = {
-    WELCOME_EMAIL,
-    GOAL_ADDED,
-    REMAIND_GOAL
-}
+  WELCOME_EMAIL,
+  GOAL_ADDED,
+  REMAIND_GOAL,
+};
--- backend/model/postModel.js
+++ backend/model/postModel.js
@@ -1,27 +1,30 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const commentSchema = require('./commentModel')
+const commentSchema = require("./commentModel");
 
-const postSchema = mongoose.Schema({
-
-    user:{
-        type: String,
-        required: true,
+const postSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
     },
     desc: {
-        type: String,
-        max: 280,
+      type: String,
+      max: 280,
     },
-    img:{
-        type:String,
-        required: [true, 'please add an image to your post']
+    img: {
+      type: String,
+      required: [true, "please add an image to your post"],
     },
-    likes:{
-        type:Array,
-        default:[]
-    }
-},{
+    likes: {
+      type: Array,
+      default: [],
+    },
+  },
+  {
     timestamps: true,
-})
+  }
+);
 
-module.exports = mongoose.model('Post', postSchema)
+module.exports = mongoose.model("Post", postSchema);
